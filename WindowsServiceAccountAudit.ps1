d1a97071be3af7bcdcba830c4099df4d595a286e
keteague <kteague+github.com@pobox.com>
2022-01-27 11:03:24 -0600

    Add files via upload

---
+++ WindowsServiceAccountAudit.ps1
@@ -0,0 +1,4 @@
+﻿$users = Get-Content -Path 'C:\Temp\userlist.txt'
+ForEach ($user in $users) {
+    Get-WmiObject -ComputerName "ex2" Win32_Service -Filter "StartName Like '%$user'" | Format-Table Name, StartName
+}
